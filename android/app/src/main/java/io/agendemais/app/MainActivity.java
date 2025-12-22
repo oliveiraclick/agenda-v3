4edb20689725704928be1582e0830fa6bdd2f5ba
Denys <denyscoborges@gmail.com>
2025-12-22 12:35:41 -0300

    add android build files and retry deployment

---
+++ android/app/src/main/java/io/agendemais/app/MainActivity.java
@@ -0,0 +1,5 @@
+package io.agendemais.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.agendemais.app',
+  appName: 'Agende Mais',
+  webDir: 'dist'
+};
+
+export default config;
